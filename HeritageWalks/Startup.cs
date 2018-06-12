5d7d66354e450b2848238ca42497aa5cbc59d6fa
keunaa88 <keunaa88@gmail.com>
2018-06-12 16:33:56 +0800

    update

---
+++ HeritageWalks/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HeritageWalks.Startup))]
+namespace HeritageWalks
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
